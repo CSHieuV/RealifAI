fdee3b6f285b6506d40242c8c27f950f407730ac
Ethan <eethanccoon@gmail.com>
2023-09-16 14:14:20 -0400

    Created renderMap function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,11 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
-import {alpha, AppBar, Button, InputAdornment, InputBase, styled, TextField, Toolbar} from "@mui/material";
+import { alpha, AppBar, Button, InputAdornment, InputBase, styled, TextField, Toolbar } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
-import {useState} from 'react';
+import { useState } from 'react';
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 function Footer() {
   return (
@@ -58,6 +60,25 @@ function SearchBar() {
 
     )
 
+}
+
+function RenderMap() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+}
+
+function Map() {
+  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+
+  return (
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <Marker position={center} />
+    </GoogleMap>
+  );
 }
 
 export default function App() {
