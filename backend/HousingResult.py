a591745cc5fb3c64f08e5cab95a96e96b8b96076
Ethan Ermovick <keg9ve@virginia.edu>
2023-09-17 00:08:09 -0400

    start description

--- backend/HousingResult.py
+++ backend/HousingResult.py
@@ -7,3 +7,9 @@ class HousingResult:
     longitude: float
     latitude: float
     other_data: Dict[str, str]
+    description: str
+
+    def __init__(self, longitude, latitude, other_data):
+        self.longitude = longitude
+        self.latitude = latitude
+        self.other_data = other_data
