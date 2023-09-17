afba3c87ff868d364673ef3a51fb00db91f5554f
Arti-Dev <boomcreeper296@gmail.com>
2023-09-17 00:30:26 -0400

    Custom bar for StreetView

--- frontend/src/StreetViewPage.js
+++ frontend/src/StreetViewPage.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactStreetview from 'react-streetview';
 import getGoogleMapsAPIKey from './ApiKeys'
+import {AppBar, IconButton, Toolbar} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import Typography from "@mui/material/Typography";
+import {ArrowBack} from "@mui/icons-material";
+
+function DescriptionBar() {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton href={'.'} edge="start" color="inherit" aria-label="home">
+                    <HomeIcon />
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    NLPRealEstate
+                </Typography>
+                <Typography variant="p" component="div" sx={{ flexGrow: 1 }}>
+                    Placeholder text
+                </Typography>
+                <IconButton edge="end" color="inherit" aria-label="back">
+                    <ArrowBack />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+    );
+}
 
 export default function StreetViewPage() {
     const googleMapsApiKey = getGoogleMapsAPIKey()
@@ -14,9 +39,10 @@ export default function StreetViewPage() {
     return (
         <div style={{
             width: '100%',
-            height: '100vh',
+            height: '91.3vh',
             backgroundColor: '#eeeeee'
         }}>
+            <DescriptionBar/>
             <ReactStreetview
                 apiKey={googleMapsApiKey}
                 streetViewPanoramaOptions={streetViewPanoramaOptions}
